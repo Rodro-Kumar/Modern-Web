e86a451cfc70ba0f9c996cd26b5cb586fe463e59
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-21 19:13:05 +0600

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./Component/Nav";
+import Hero from "./Component/Hero";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
