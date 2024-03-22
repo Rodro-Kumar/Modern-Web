2119ca9aecd2d472d2842f8db22a419c7fd26174
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-22 20:19:17 +0600

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Nav from "./Component/Nav";
 import Hero from "./Component/Hero";
+import Marquee from "./Component/Marquee";
+import About from "./Component/About";
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Marquee />
+      <About />
     </>
   );
 }
