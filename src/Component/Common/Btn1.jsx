e5886f216a9b00cd0b2b9de6925c3abd293a7993
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-23 15:35:51 +0600

    common btn

---
+++ src/Component/Common/Btn1.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { MdArrowOutward } from "react-icons/md";
+
+const Btn1 = ({ className, title }) => {
+  return (
+    <>
+      <div className="group flex items-center gap-x-2 cursor-pointer">
+        <a
+          href="#"
+          className={`uppercase text-[16px] font-normal py-1 px-3 rounded-full border-2 border-[#000] group-hover:bg-black group-hover:text-white group-hover:transition ease  ${className}`}
+        >
+          {title ? title : "Start the Project"}
+        </a>
+        <div className="arrow w-[32px] h-[32px] border-2 border-black rounded-full flex items-center justify-center cursor-pointer group-hover:bg-black group-hover:text-white group-hover:transition-colors">
+          <MdArrowOutward className="text-xl" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Btn1;
