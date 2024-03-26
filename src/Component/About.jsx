9bbd22b867ea7ff064160e15a222164173e7a702
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-26 11:33:08 +0600

    About

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { GoArrowUpRight } from "react-icons/go";
+import approachImg from "../assets/approch-img.jpg";
 
 const About = () => {
   return (
     <>
-      <div className="bg-[#cdea68] rounded-xl py-20">
+      <div className="bg-[#cdea68] rounded-xl pt-20 pb-4">
         <div className="pb-14 border-b border-[#00000048]">
           <div className="container">
             <h1 className="max-w-[1088px] text-[52px] text-black font-normal font-['Neue_Montreal'] leading-[52px]">
@@ -73,6 +75,35 @@ const About = () => {
                 >
                   Linkedin
                 </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="container">
+            <div className="flex justify-between pb-12">
+              <div>
+                <h4 className="text-[52px] font-['Neue_Montreal'] font-normal leading-none mb-7">
+                  Our approach:
+                </h4>
+                <button className="group readMoreBtn flex items-center bg-black py-3 pl-5 pr-2 rounded-full hover:bg-[#1d1d1d]">
+                  <a
+                    href="#"
+                    className="text-base font-normal text-white font-['Neue_Montreal']"
+                  >
+                    READ MORE
+                  </a>
+                  <div className=" w-10 h-10 scale-[0.3] bg-white rounded-full flex items-center justify-center text-xl ml-7 group-hover:scale-[1] transition-all">
+                    <GoArrowUpRight className="hidden group-hover:block" />
+                  </div>
+                </button>
+              </div>
+              <div className="max-w-[587px] mt-3">
+                <img
+                  src={approachImg}
+                  alt={approachImg}
+                  className="rounded-xl"
+                />
               </div>
             </div>
           </div>
