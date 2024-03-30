b51de54d28046aeb603c56ba8b753413f93ec7cf
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-30 15:38:23 +0600

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Marquee from "./Component/Marquee";
 import About from "./Component/About";
 import Play from "./Component/Play";
 import Feature from "./Component/Feature";
+import Reviews from "./Component/Reviews";
+import Card from "./Component/Card";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <About />
       <Play />
       <Feature />
+      <Reviews />
+      <Card />
     </>
   );
 }
