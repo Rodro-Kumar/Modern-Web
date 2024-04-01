64784e17df4b70726ec63bddf3dbf4757325174d
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-01 11:32:18 +0600

    modified

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = () => {
   return (
     <>
-      <div>
+      <div className="fixed z-[999] w-full top-0">
         <div className="container py-4 flex justify-between items-center">
           <div className="cursor-pointer">
             <svg
