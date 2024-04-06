2187558192d061b13d109e37206315e9ad18ac26
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-06 11:30:59 +0600

    modified

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoArrowUpRight } from "react-icons/go";
 import approachImg from "../assets/approch-img.jpg";
 
 const About = () => {
+  const [hovering, sethovering] = useState(false);
+
   return (
     <>
-      <div className="bg-[#cdea68] rounded-xl pt-20 pb-4">
+      <div
+        data-scroll
+        data-scroll-section
+        data-scroll-speed="-.4"
+        className="bg-[#cdea68] rounded-xl pt-20 pb-4 about"
+      >
         <div className="pb-14 border-b border-[#00000048]">
           <div className="container">
             <h1 className="max-w-[1088px] text-[52px] text-black font-normal font-['Neue_Montreal'] leading-[52px]">
               Ochi is a strategic partner for fast-grow­ing tech businesses that
               need to{" "}
-              <span className="border-b-4 border-black cursor-pointer">
-                raise funds,
-              </span>{" "}
-              <span className="border-b-4 border-black cursor-pointer">
+              <span className=" cursor-pointer under-hover">raise funds,</span>{" "}
+              <span className=" cursor-pointer under-hover">
                 sell prod­ucts,
               </span>{" "}
-              <span className="border-b-4 border-black cursor-pointer">
+              <span className="cursor-pointer under-hover">
                 ex­plain com­plex ideas,
               </span>{" "}
               and{" "}
-              <span className="border-b-4 border-black cursor-pointer">
+              <span className=" cursor-pointer under-hover">
                 hire great peo­ple.
               </span>
             </h1>
           </div>
         </div>
-        <div className="border-b border-[#00000048] pt-4 pb-28">
+        <div className="border-b border-[#00000048] pt-4 pb-28 common">
           <div className="container flex justify-between">
             <div>
               <p className="font-['Neue_Montreal'] text-base font-normal text-black">
@@ -47,49 +52,62 @@ const About = () => {
                   captivating.
                 </p>
               </div>
-              <div className="flex flex-col justify-end">
+              <ul className="flex flex-col justify-end">
                 <p className="font-['Neue_Montreal'] text-base font-normal text-black pb-5 ">
                   S:
                 </p>
-                <a
-                  href="#"
-                  className="font-['Neue_Montreal'] text-base font-normal text-black border-b border-black "
-                >
-                  Instagram
-                </a>
-                <a
-                  href="#"
-                  className="font-['Neue_Montreal'] text-base font-normal text-black border-b border-black"
-                >
-                  Behance
-                </a>
-                <a
-                  href="#"
-                  className="font-['Neue_Montreal'] text-base font-normal text-black border-b border-black"
-                >
-                  Facebook
-                </a>
-                <a
-                  href="#"
-                  className="font-['Neue_Montreal'] text-base font-normal text-black border-b border-black "
-                >
-                  Linkedin
-                </a>
-              </div>
+                <li>
+                  {" "}
+                  <a
+                    href="#"
+                    className="font-['Neue_Montreal'] text-base font-normal text-black "
+                  >
+                    Instagram
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="font-['Neue_Montreal'] text-base font-normal text-black "
+                  >
+                    Behance
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="font-['Neue_Montreal'] text-base font-normal text-black "
+                  >
+                    Facebook
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="font-['Neue_Montreal'] text-base font-normal text-black "
+                  >
+                    Linkedin
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
         <div>
           <div className="container">
-            <div className="flex justify-between pb-12">
-              <div>
+            <div className="approach flex justify-between pb-12">
+              <div href="#">
                 <h4 className="text-[52px] font-['Neue_Montreal'] font-normal leading-none mb-7">
                   Our approach:
                 </h4>
-                <button className="group readMoreBtn flex items-center bg-black py-3 pl-5 pr-2 rounded-full hover:bg-[#1d1d1d]">
+                <button
+                  onMouseEnter={() => sethovering(true)}
+                  onMouseLeave={() => sethovering(false)}
+                  className="group readMoreBtn flex items-center bg-black py-3 pl-5 pr-2 rounded-full hover:bg-[#1d1d1d]"
+                >
                   <a
                     href="#"
-                    className="text-base font-normal text-white font-['Neue_Montreal']"
+                    className="text-base font-normal text-white font-['Neue_Montreal'] inline-block"
                   >
                     READ MORE
                   </a>
@@ -98,11 +116,15 @@ const About = () => {
                   </div>
                 </button>
               </div>
-              <div className="max-w-[587px] mt-3">
+              <div
+                className={`imgContainer h-[420px] w-[587px] mt-3 bg-red-700 rounded-xl overflow-hidden ${
+                  hovering ? "scale-95" : null
+                }`}
+              >
                 <img
                   src={approachImg}
                   alt={approachImg}
-                  className="rounded-xl"
+                  className={`w-full h-full ${hovering ? "scale-110" : null}`}
                 />
               </div>
             </div>
