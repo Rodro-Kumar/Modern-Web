62b05c4b51d2d0462586e2c83e9803032fd0c783
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-08 13:45:18 +0600

    modified

--- src/Component/Common/Btn2.jsx
+++ src/Component/Common/Btn2.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Btn2 = ({ className, BtnTitle }) => {
   return (
     <button
-      className={`btn2 py-[5px] px-[14px] bg-transparent text-base text-black border-2 border-black rounded-full font-['Neue_Montreal'] font-normal uppercase ${className}`}
+      className={`btn2 py-[5px] px-[14px] bg-transparent text-base text-black border-[1.5px] border-black rounded-full font-['Neue_Montreal'] font-normal uppercase whitespace-nowrap ${className}`}
     >
       <a href="#">{BtnTitle ? BtnTitle : "button"}</a>
     </button>
