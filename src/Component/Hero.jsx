32508b8c16a0e2eb64550194473ccea04b8e9cfe
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-07-04 12:32:06 +0600

    modified

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
       <div id="hero" className="mt-[112px]">
         <div className="container">
           {/* =============== */}
-          <div className="heading px-4 xl:0 block sm:hidden">
+          <div className="heading xl:0 block px-4 sm:hidden">
             {["WE CREATE", "EYE-OPENING", "PRESENTATIONS"].map(
               (item, index) => {
                 return (
                   <div key={index}>
-                    <div className="flex items-start flex-col md:flex-row md:items-center">
+                    <div className="flex flex-col items-start md:flex-row md:items-center">
                       {index === 1 && (
                         <motion.div
                           initial={{ width: 0 }}
@@ -22,7 +22,7 @@ const Hero = () => {
                             ease: [0.76, 0, 0.24, 1],
                             duration: 1,
                           }}
-                          className="w-[50px] sm:w-36 h-[60px] md:h-[96px] bg-[url('./src/assets/content-image01.jpg')] bg-cover bg-center mb-2 rounded-lg mr-3"
+                          className="heroOchi mb-2 mr-3 h-[60px] w-[50px] rounded-lg bg-cover bg-center sm:w-36 md:h-[96px]"
                         ></motion.div>
                       )}
                       <motion.h1
@@ -32,18 +32,18 @@ const Hero = () => {
                           ease: [0.76, 0, 0.24, 1],
                           duration: 1,
                         }}
-                        className="font-FjallaOne text-[40px] sm:text-[60px] md:text-[100px] lg:text-[108px] leading-[8vw] lg:leading-7 font-semibold text-black max-[480px]:leading-[10vw] max-[370px]:leading-[12vw] max-[315px]:leading-[15vw]"
+                        className="font-FjallaOne text-[40px] font-semibold leading-[8vw] text-black max-[480px]:leading-[10vw] max-[370px]:leading-[12vw] max-[315px]:leading-[15vw] sm:text-[60px] md:text-[100px] lg:text-[108px] lg:leading-7"
                       >
                         {item}
                       </motion.h1>
                     </div>
                   </div>
                 );
-              }
+              },
             )}
           </div>
           {/* ================== */}
-          <div className="heading px-4 xl:0 hidden sm:block">
+          <div className="heading xl:0 hidden px-4 sm:block">
             {["WE CREATE", "EYE-OPENING", "PRESENTATIONS"].map(
               (item, index) => {
                 return (
@@ -57,22 +57,22 @@ const Hero = () => {
                             ease: [0.76, 0, 0.24, 1],
                             duration: 1,
                           }}
-                          className="w-[50px] sm:w-36 h-[60px] md:h-[96px] bg-[url('https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg')] bg-cover bg-center mb-2 rounded-lg mr-3"
+                          className="mb-2 mr-3 h-[60px] w-[50px] rounded-lg bg-[url('https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg')] bg-cover bg-center sm:w-36 md:h-[96px]"
                         ></motion.div>
                       )}
-                      <motion.h1 className="font-FjallaOne text-[40px] sm:text-[60px] md:text-[100px] lg:text-[108px] leading-[12vw] lg:leading-[8.5vw] font-semibold text-black">
+                      <motion.h1 className="font-FjallaOne text-[40px] font-semibold leading-[12vw] text-black sm:text-[60px] md:text-[100px] lg:text-[108px] lg:leading-[8.5vw]">
                         {item}
                       </motion.h1>
                     </div>
                   </div>
                 );
-              }
+              },
             )}
           </div>
           {/* ====================== */}
         </div>
-        <div className="hero-line border-t-[1px] border-[#00000044] mt-[100px] py-4 md:mt-[256px]">
-          <div className="container px-4 xl:px-0 gap-y-6 sm:gap-y-0 flex justify-between items-start flex-col sm:flex-row sm:items-center">
+        <div className="hero-line mt-[100px] border-t-[1px] border-[#00000044] py-4 md:mt-[256px]">
+          <div className="container flex flex-col items-start justify-between gap-y-6 px-4 sm:flex-row sm:items-center sm:gap-y-0 xl:px-0">
             {[
               "For public and private companies",
               "From the first pitch to IPO",
